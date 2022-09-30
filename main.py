4e1fc774e8042f93247d829acfe5ecd2b84a7f13
Farangis-Toirjonova <wiut00014055@gmail.com>
2022-09-30 15:03:45 +0500

    init: seminar 3 converter

---
+++ main.py
@@ -0,0 +1,39 @@
+y = int("0b01010", 2)
+
+print(y)
+
+
+hex_num1 = "0x10"
+hex_num2 = "0x10"
+my_sum = int(hex_num1, 16) + int(hex_num2, 16)
+print(my_sum)
+print(hex(my_sum))
+
+
+#task5
+
+def decimal():
+    def converttodecimal(number, base):
+        #y = int(input("your number: "))
+        temp = int(number, base)
+        print(temp)
+
+    y = (input("number: "))
+    x = int(input("base: "))
+
+    converttodecimal(y, x)
+
+
+#task6
+
+
+
+
+
+
+
+
+
+
+
+
